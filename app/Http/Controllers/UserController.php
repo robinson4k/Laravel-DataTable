d380db40d198280008636cddb78749b971a29b86
Robinson Silva <robinson4k@gmail.com>
2018-09-28 23:02:02 -0400

    removido foreach e adicionado map para eloquent de users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,17 +14,8 @@ class UserController extends Controller
 
     public function allusers(Request $request)
     {
-        
-
-        
-        $columns = array( 
-                            0 =>'id', 
-                            1 =>'name',
-                            2=> 'email'
-                        );
-  
+        $columns = ['id', 'name', 'email'];
         $totalData = User::count();
-            
         $totalFiltered = $totalData; 
 
         $limit = $request->input('length');
@@ -32,53 +23,25 @@ class UserController extends Controller
         $order = $columns[$request->input('order.0.column')];
         $dir = $request->input('order.0.dir');
             
-        if(empty($request->input('search.value')))
-        {            
-            $users = User::offset($start)
-                         ->limit($limit)
-                         ->orderBy($order,$dir)
-                         ->get();
-        }
+        if (empty($request->input('search.value')))
+            $users = User::offset($start)->limit($limit)->orderBy($order,$dir)->get();
         else {
             $search = $request->input('search.value'); 
+            $users =  User::where('id','LIKE',"%{$search}%")->orWhere('name', 'LIKE',"%{$search}%")->offset($start)->limit($limit)->orderBy($order,$dir)->get();
 
-            $users =  User::where('id','LIKE',"%{$search}%")
-                            ->orWhere('name', 'LIKE',"%{$search}%")
-                            ->offset($start)
-                            ->limit($limit)
-                            ->orderBy($order,$dir)
-                            ->get();
-
-            $totalFiltered = User::where('id','LIKE',"%{$search}%")
-                             ->orWhere('name', 'LIKE',"%{$search}%")
-                             ->count();
+            $totalFiltered = User::where('id','LIKE',"%{$search}%")->orWhere('name', 'LIKE',"%{$search}%")->count();
         }
 
-        $data = array();
-        if(!empty($users))
-        {
-            foreach ($users as $post)
-            {
-                $show =  route('users.show',$post->id);
-                $edit =  route('users.edit',$post->id);
+        $users = $users->map(function($set) {
+            $set['email'] = "<a href=''>".$set->email."</a>";
+            return $set;
+        });
 
-                $nestedData['id'] = $post->id;
-                $nestedData['name'] = $post->name;
-                $nestedData['email'] = $post->email;
-                $data[] = $nestedData;
-
-            }
-        }
-          
-        $json_data = array(
-                    "draw"            => intval($request->input('draw')),  
-                    "recordsTotal"    => intval($totalData),  
-                    "recordsFiltered" => intval($totalFiltered), 
-                    "data"            => $data   
-                    );
-            
-        echo json_encode($json_data);
-
-
+        echo json_encode([
+            "draw" => intval($request->input('draw')),
+            "recordsTotal" => intval($totalData),
+            "recordsFiltered" => intval($totalFiltered),
+            "data" => $users->toArray()
+        ]);
     }
 }
